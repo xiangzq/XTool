089c279b36de880f700307984816dda38ad82220
项正强 <xzq@xiangzhengqiangdeMac-mini.local>
2021-02-18 14:37:27 +0800

    1.0.8

--- XTool/Classes/NSArray+XTool.h
+++ XTool/Classes/NSArray+XTool.h
@@ -6,11 +6,19 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface NSArray (XTool)
 
+@end
+
+@interface NSArray (XImageData)
+/// 将UIImage类型的数组转为NSData类型的数组
+- (NSArray<NSData *> *) x_datas;
+/// 将NSData类型的数组转为UIImage类型的数组
+- (NSArray<UIImage *> *) x_images;
 @end
 
 NS_ASSUME_NONNULL_END
--- XTool/Classes/NSArray+XTool.m
+++ XTool/Classes/NSArray+XTool.m
@@ -10,3 +10,27 @@
 @implementation NSArray (XTool)
 
 @end
+
+@implementation NSArray (XImageData)
+- (NSArray<NSData *> *)x_datas {
+    NSMutableArray<NSData *> *datas = [NSMutableArray arrayWithCapacity:self.count];
+    [self enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        if ([obj isKindOfClass:[UIImage class]]) {
+            NSData *data = UIImagePNGRepresentation((UIImage *)obj);
+            [datas addObject:data];
+        }
+    }];
+    return datas;
+}
+
+- (NSArray<UIImage *> *)x_images {
+    NSMutableArray<UIImage *> *images = [NSMutableArray arrayWithCapacity:self.count];
+    [self enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        if ([obj isKindOfClass:[NSData class]]) {
+            UIImage *image = [UIImage imageWithData:(NSData *)obj];
+            [images addObject:image];
+        }
+    }];
+    return images;
+}
+@end
