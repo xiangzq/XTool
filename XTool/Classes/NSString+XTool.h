2dacbbe3ccf85f0a8f67020838bffc59400e959e
xiangac <xiangac1992@163.com>
2021-10-06 09:09:26 +0800

    1.1.2

--- XTool/Classes/NSString+XTool.h
+++ XTool/Classes/NSString+XTool.h
@@ -10,7 +10,12 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface NSString (XTool)
-
+/// 判断是否是纯汉字
+- (BOOL)isChinese;
+/// 判断是否含有汉字
+- (BOOL)includeChinese;
+/// 判断是否含有html标签
+- (BOOL) hasHtmlLabel;
 @end
 
 NS_ASSUME_NONNULL_END
--- XTool/Classes/NSString+XTool.m
+++ XTool/Classes/NSString+XTool.m
@@ -9,4 +9,25 @@
 
 @implementation NSString (XTool)
 
+- (BOOL) isChinese {
+    NSString *match = @"(^[\u4e00-\u9fa5]+$)";
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF matches %@", match];
+    return [predicate evaluateWithObject:self];
+}
+
+- (BOOL)includeChinese {
+    for (int i = 0; i < [self length]; i++) {
+        int a = [self characterAtIndex:i];
+        if( a > 0x4e00 && a < 0x9fff ) return YES;
+    }
+    return NO;
+}
+
+- (BOOL) hasHtmlLabel {
+    if ([self containsString:@"<"] && [self containsString:@"</"] && [self containsString:@">"]) {
+        return true;
+    }
+    return false;
+}
+
 @end
